f89f2aee87896ae333ae4b6e83b375841791aba9
WilliCarvalho <willian_matheus14@hotmail.com>
2020-05-13 20:36:06 -0300

    Initial commit

---
+++ Assets/Scripts/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Configuration : MonoBehaviour
+{
+    public GameObject trunk;
+    public float basePosition;
+    public int initialQuantity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < initialQuantity; i++)
+        {        
+            GameObject currentTrunk = Instantiate(trunk, new Vector2(0f, basePosition), transform.rotation);
+
+            currentTrunk.transform.position = new Vector2(currentTrunk.transform.position.x, 
+                transform.position.y + basePosition);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //private float xStart;
+    //private float xEnd;
+    //private string currentSide;
+    private Animator _animator;
+    private SpriteRenderer sprite;
+    private float positionX;
+    private bool canAnimate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        positionX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Swipe();
+    }
+
+    void Swipe()
+    {
+        
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(Input.mousePosition.x > Screen.width / 2)
+            {
+                //right
+                transform.position = new Vector2(-positionX, transform.position.y);
+                sprite.flipX = true;
+            }
+            else if(Input.mousePosition.x < Screen.width / 2)
+            {
+                //left
+                transform.position = new Vector2(positionX, transform.position.y);
+                sprite.flipX = false;
+            }
+
+            //Animation
+            StartCoroutine(Animate());
+        }
+
+        //Another way to detect the position of the character
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    xStart = Input.mousePosition.x;            
+        //}
+        //else if (Input.GetButtonUp("Fire1"))
+        //{
+        //    xEnd = Input.mousePosition.x;
+
+        //    if(xStart < xEnd)
+        //    {
+        //        transform.position = new Vector2(3f, transform.position.y);
+        //    }
+        //    else
+        //    {
+        //        transform.position = new Vector2(-3f, transform.position.y);
+        //    }
+        //}
+    }
+
+
+    IEnumerator Animate()
+    {
+        _animator.SetBool("pCutting", true);
+        yield return new WaitForSeconds(0.35f);
+        _animator.SetBool("pCutting", false);
+    }
+}
